1af6a064eaf36af29940758d8dba987dcd60b91d
thyonix <dongf7889@gmail.com>
2025-03-20 18:11:53 +0800

    [frontend] install elementPlus and icons

--- shop-admin-frontend/src/main.ts
+++ shop-admin-frontend/src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './assets/style/globalStyle.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// use elementplus and icons
+app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.mount('#app')
